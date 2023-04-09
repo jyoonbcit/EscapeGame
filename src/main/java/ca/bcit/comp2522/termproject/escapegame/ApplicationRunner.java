2d33288184fde31f438eebbefef3e4fedc13ff5d
Wilbster <95505563+Wilbster@users.noreply.github.com>
2023-04-09 12:57:24 -0700

    remove extraneous import

--- src/main/java/ca/bcit/comp2522/termproject/escapegame/ApplicationRunner.java
+++ src/main/java/ca/bcit/comp2522/termproject/escapegame/ApplicationRunner.java
@@ -6,7 +6,6 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
-import javafx.scene.media.MediaView;
 import javafx.stage.Stage;
 
 import java.io.File;
